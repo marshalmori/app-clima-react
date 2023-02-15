d1691b088668496623a162fb03b07a5bbf9a43e4
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-02-15 14:38:58 -0300

    Initial commit

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+import Formulario from "./Formulario";
+
+const AppClima = () => {
+  return (
+    <Fragment>
+      <main>
+        <Formulario />
+      </main>
+    </Fragment>
+  );
+};
+
+export default AppClima;
